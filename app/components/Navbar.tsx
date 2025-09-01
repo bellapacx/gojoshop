027bcb2662b53d79263aee24179fd191cadd5d9b
sunvilacoin <sunvilacoin@gmail.com>
2025-09-01 20:46:59 +0300

    Fresh startddsdsssdddaddddkhsskkllkeesss

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,11 +1,20 @@
 "use client";
 import { Menu, User } from "lucide-react";
+import { useEffect, useState } from "react";
 
 interface NavbarProps {
   onMenuClick: () => void;
 }
+  
 
 export function Navbar({ onMenuClick }: NavbarProps) {
+  const [storedShopId,setStoredShopId] = useState<string | null>(null);
+  useEffect(() => {
+    const storedShopId = localStorage.getItem("shop_id");
+    if(storedShopId){
+      setStoredShopId(storedShopId);
+    }
+  }, []);
   return (
     <header className="h-16 bg-white shadow-sm flex items-center justify-between px-4 md:px-6">
       {/* Left side (Hamburger + Title) */}
@@ -24,7 +33,7 @@ export function Navbar({ onMenuClick }: NavbarProps) {
       {/* Right side (User Profile) */}
       <div className="flex items-center gap-4">
         <span className="hidden sm:block text-slate-600 font-medium">
-          Admin
+          {storedShopId}
         </span>
         <button className="p-2 rounded-full bg-slate-100 hover:bg-slate-200">
           <User className="w-5 h-5 text-slate-700" />
